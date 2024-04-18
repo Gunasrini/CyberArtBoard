aa72bf092d279f9b3b703ce032c57c7af5355c48
Gunasekaran Srinivasan <gunasekaran.srinivasan@kanini.com>
2024-04-18 15:22:32 +0530

    latest changes

--- src/components/BillBoard/BillBoard.js
+++ src/components/BillBoard/BillBoard.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../BillBoard/BillBoard.css';
 import generateImg1 from '../../assets/images/generateImg1.png';
 import generateImg2 from '../../assets/images/generateImg2.png';
@@ -19,6 +19,11 @@ import billBoardImg12 from '../../assets/images/Design BillBoard/img12.png';
 import Img2DesignModal from '../../modals/Img2DesignModal';
 
 export default function BillBoard() {
+    const [getBillboardimage,setgetBillboardimage]=useState('');
+
+    const BillBoardImage=(src)=>{
+        setgetBillboardimage(src)
+    }
     return (
         <div className='dreams-section billboard-section mb-0'>
             <div className='container'>
@@ -27,62 +32,62 @@ export default function BillBoard() {
                 </div>
                 <div className='row'>
                     <div className='col-md-3 list-item'>
-                        <img src={generateImg1} alt="" data-bs-toggle="modal" data-bs-target="#img2DesignModal" />
+                        <img src={generateImg1} alt="" onClick={()=>BillBoardImage(generateImg1)} data-bs-toggle="modal" data-bs-target="#img2DesignModal" />
                     </div>
                     <div className='col-md-3 list-item'>
-                        <img src={generateImg2} alt="" data-bs-toggle="modal" data-bs-target="#img2DesignModal" />
+                        <img src={generateImg2} alt="" onClick={()=>BillBoardImage(generateImg2)} data-bs-toggle="modal" data-bs-target="#img2DesignModal" />
                     </div>
                     <div className='col-md-3 list-item'>
-                        <img src={generateImg3} alt="" data-bs-toggle="modal" data-bs-target="#img2DesignModal" />
+                        <img src={generateImg3} alt="" onClick={()=>BillBoardImage(generateImg3)} data-bs-toggle="modal" data-bs-target="#img2DesignModal" />
                     </div>
                     <div className='col-md-3 list-item'>
-                        <img src={generateImg4} alt="" data-bs-toggle="modal" data-bs-target="#img2DesignModal" />
-                    </div>
-                </div>
-                <div className='row'>
-                    <div className='col-md-3 list-item'>
-                        <img src={billBoardImg1} alt="" data-bs-toggle="modal" data-bs-target="#img2DesignModal" />
-                    </div>
-                    <div className='col-md-3 list-item'>
-                        <img src={billBoardImg2} alt="" data-bs-toggle="modal" data-bs-target="#img2DesignModal" />
-                    </div>
-                    <div className='col-md-3 list-item'>
-                        <img src={billBoardImg3} alt="" data-bs-toggle="modal" data-bs-target="#img2DesignModal" />
-                    </div>
-                    <div className='col-md-3 list-item'>
-                        <img src={billBoardImg4} alt="" data-bs-toggle="modal" data-bs-target="#img2DesignModal" />
+                        <img src={generateImg4} alt="" onClick={()=>BillBoardImage(generateImg4)} data-bs-toggle="modal" data-bs-target="#img2DesignModal" />
                     </div>
                 </div>
                 <div className='row'>
                     <div className='col-md-3 list-item'>
-                        <img src={billBoardImg5} alt="" data-bs-toggle="modal" data-bs-target="#img2DesignModal" />
+                        <img src={billBoardImg1} alt="" onClick={()=>BillBoardImage(billBoardImg1)} data-bs-toggle="modal" data-bs-target="#img2DesignModal" />
                     </div>
                     <div className='col-md-3 list-item'>
-                        <img src={billBoardImg6} alt="" data-bs-toggle="modal" data-bs-target="#img2DesignModal" />
+                        <img src={billBoardImg2} alt="" onClick={()=>BillBoardImage(billBoardImg2)} data-bs-toggle="modal" data-bs-target="#img2DesignModal" />
                     </div>
                     <div className='col-md-3 list-item'>
-                        <img src={billBoardImg7} alt="" data-bs-toggle="modal" data-bs-target="#img2DesignModal" />
+                        <img src={billBoardImg3} alt="" onClick={()=>BillBoardImage(billBoardImg3)} data-bs-toggle="modal" data-bs-target="#img2DesignModal" />
                     </div>
                     <div className='col-md-3 list-item'>
-                        <img src={billBoardImg8} alt="" data-bs-toggle="modal" data-bs-target="#img2DesignModal" />
+                        <img src={billBoardImg4} alt="" onClick={()=>BillBoardImage(billBoardImg4)} data-bs-toggle="modal" data-bs-target="#img2DesignModal" />
                     </div>
                 </div>
                 <div className='row'>
                     <div className='col-md-3 list-item'>
-                        <img src={billBoardImg9} alt="" data-bs-toggle="modal" data-bs-target="#img2DesignModal" />
+                        <img src={billBoardImg5} alt="" onClick={()=>BillBoardImage(billBoardImg5)} data-bs-toggle="modal" data-bs-target="#img2DesignModal" />
                     </div>
                     <div className='col-md-3 list-item'>
-                        <img src={billBoardImg10} alt="" data-bs-toggle="modal" data-bs-target="#img2DesignModal" />
+                        <img src={billBoardImg6} alt="" onClick={()=>BillBoardImage(billBoardImg5)} data-bs-toggle="modal" data-bs-target="#img2DesignModal" />
                     </div>
                     <div className='col-md-3 list-item'>
-                        <img src={billBoardImg11} alt="" data-bs-toggle="modal" data-bs-target="#img2DesignModal" />
+                        <img src={billBoardImg7} alt="" onClick={()=>BillBoardImage(billBoardImg7)} data-bs-toggle="modal" data-bs-target="#img2DesignModal" />
                     </div>
                     <div className='col-md-3 list-item'>
-                        <img src={billBoardImg12} alt="" data-bs-toggle="modal" data-bs-target="#img2DesignModal" />
+                        <img src={billBoardImg8} alt="" onClick={()=>BillBoardImage(billBoardImg8)} data-bs-toggle="modal" data-bs-target="#img2DesignModal" />
+                    </div>
+                </div>
+                <div className='row'>
+                    <div className='col-md-3 list-item'>
+                        <img src={billBoardImg9} alt="" onClick={()=>BillBoardImage(billBoardImg9)} data-bs-toggle="modal" data-bs-target="#img2DesignModal" />
+                    </div>
+                    <div className='col-md-3 list-item'>
+                        <img src={billBoardImg10} alt="" onClick={()=>BillBoardImage(billBoardImg10)} data-bs-toggle="modal" data-bs-target="#img2DesignModal" />
+                    </div>
+                    <div className='col-md-3 list-item'>
+                        <img src={billBoardImg11} alt="" onClick={()=>BillBoardImage(billBoardImg11)} data-bs-toggle="modal" data-bs-target="#img2DesignModal" />
+                    </div>
+                    <div className='col-md-3 list-item'>
+                        <img src={billBoardImg12} alt="" onClick={()=>BillBoardImage(billBoardImg12)} data-bs-toggle="modal" data-bs-target="#img2DesignModal" />
                     </div>
                 </div>
             </div>
-            <Img2DesignModal />
+            <Img2DesignModal imageSrc={getBillboardimage}/>
         </div>
     )
 }
--- src/modals/Img2DesignModal.js
+++ src/modals/Img2DesignModal.js
@@ -2,9 +2,19 @@ import React from 'react'
 // import generateImg from '../assets/images/generateImg1.png';
 import iconImg from '../assets/images/icon.png';
 import { useNavigate } from 'react-router-dom';
+import {
+    FacebookShareButton,
+    TwitterShareButton,
+    PinterestShareButton,
+    LinkedinShareButton,
+    EmailShareButton,
+  } from 'react-share';
 
 export default function Img2DesignModal(props) {
-
+    console.log("textttttttttttttttt",props);
+    const shareUrl = 'https://example.com';
+  const title = 'Check out this awesome content!';
+  const media = 'https://example.com/image.jpg';
     const navigate = useNavigate();
 
     const redirectToOrderPage = (e) => {
@@ -12,6 +22,31 @@ export default function Img2DesignModal(props) {
         navigate(`/products/order`);
         navigate('/products/order', { state: { image: props.imageSrc } });
         }
+
+        const socialMediaShare = (url) => {
+            const currentUrl = "https://gunasrini.github.io/CyberArtBoard/"
+        
+            // Copy current page URL to clipboard
+            navigator.clipboard.writeText(currentUrl)
+              .then(() => {
+                // Open Facebook share dialog with copied URL
+                window.open(url + encodeURIComponent(currentUrl), '_blank');
+              })
+              .catch((error) => {
+                console.error('Failed to copy URL: ', error);
+              });
+          };
+
+          function downloadImage(){
+            const link = document.createElement('a');
+            const fileName = 'image.jpg';
+            link.href = props.imageSrc;
+            link.download = fileName;
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+  };
+          
     return (
         <>
             <div className="modal modal-md" id="img2DesignModal">
@@ -28,13 +63,16 @@ export default function Img2DesignModal(props) {
                         </div>
                         <div className='col-md-2'>
                             <div className='product-share'>
-                                <i className="fa-solid fa-download"></i>
-                                <i className="fa-brands fa-facebook"></i>
-                                <i className="fa-brands fa-instagram"></i>
-                                <i className="fa-brands fa-twitter"></i>
+                                <i className="fa-solid fa-download" onClick={downloadImage}></i>
+                                <i className="fa-brands fa-facebook" onClick={()=>socialMediaShare('https://www.facebook.com/sharer/sharer.php?u=')}>
+                                <FacebookShareButton >
+                                </FacebookShareButton>
+                                </i>
+                                <i className="fa-brands fa-instagram" onClick={()=>socialMediaShare('https://www.instagram.com/your_instagram_username/')}></i>
+                                <i className="fa-brands fa-twitter" onClick={()=>socialMediaShare('https://twitter.com/?lang=en')}></i>
                                 <i className="fa-brands fa-pinterest"></i>
-                                <i className="fa-solid fa-envelope"></i>
-                                <i className="fa-brands fa-linkedin"></i>
+                                <i className="fa-solid fa-envelope" onClick={()=>socialMediaShare('https://mail.google.com/mail')}></i>
+                                <i className="fa-brands fa-linkedin" onClick={()=>socialMediaShare('https://www.linkedin.com/sharing/share-offsite')}></i>
                             </div>
                         </div>
                         <div className='generate-btn'>
